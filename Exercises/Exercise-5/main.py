4ef4cc3a295b5ad212af392cdecc9f30ed56cabb
Casper ten Dam <caspertd97@gmail.com>
2024-10-20 12:41:38 +0200

    Add create table statement for accounts table

--- Exercises/Exercise-5/main.py
+++ Exercises/Exercise-5/main.py
@@ -1,4 +1,5 @@
 import psycopg2
+import pathlib
 
 
 def main():
@@ -8,6 +9,13 @@ def main():
     pas = "postgres"
     conn = psycopg2.connect(host=host, database=database, user=user, password=pas)
     # your code here
+    with conn.cursor() as cur:
+        # create the table
+        # TODO: consider varchar lengths, zip code to INT?, map date to date?
+        cur.execute(
+            "CREATE TABLE accounts (customer_id serial PRIMARY KEY, first_name VARCHAR, last_name VARCHAR, address_1 VARCHAR, address_2 VARCHAR, city VARCHAR, state VARCHAR, zip_code VARCHAR, join_date VARCHAR)"
+        )
+        cur.commit()
 
 
 if __name__ == "__main__":
