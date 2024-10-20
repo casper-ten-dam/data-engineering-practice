974b28b7d6028facab8fbf0a3e0f2bff42ee5d96
Casper ten Dam <caspertd97@gmail.com>
2024-10-20 12:45:37 +0200

    Attempt at csv import (doesnt work yet)

--- Exercises/Exercise-5/main.py
+++ Exercises/Exercise-5/main.py
@@ -16,6 +16,17 @@ def main():
             "CREATE TABLE accounts (customer_id serial PRIMARY KEY, first_name VARCHAR, last_name VARCHAR, address_1 VARCHAR, address_2 VARCHAR, city VARCHAR, state VARCHAR, zip_code VARCHAR, join_date VARCHAR)"
         )
         cur.commit()
+        # insert the data from the csv file
+        # TODO: fix, doesnt work as data is not in docker build
+        cur.execute(
+            """
+COPY accounts(customer_id, first_name, last_name, address_1, address_2, city, state, zip_code, join_date)
+FROM %(accounts_csv_path)s
+DELIMITER ','
+CSV HEADER;
+""",
+            {"accounts_csv_path": str(pathlib.Path.cwd() / "data" / "accounts.csv")},
+        )
 
 
 if __name__ == "__main__":
